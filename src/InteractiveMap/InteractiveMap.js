5fafec9f54112bbaa9f64f5b27144d134f6b2e03
Femke Bruckmann <femke.bruckmann@ugent.be>
2018-09-06 09:11:45 +0200

    Added Leaflet Clustering

--- src/InteractiveMap/InteractiveMap.js
+++ src/InteractiveMap/InteractiveMap.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import 'whatwg-fetch';
 import L from 'leaflet';
+import 'leaflet.markercluster';
 import './InteractiveMap.css';
 
 class InteractiveMap extends Component {
@@ -23,6 +24,7 @@ class InteractiveMap extends Component {
 
     const stations = {};
     const markers = {};
+    const markerLayer = L.markerClusterGroup();
     // const blueIcon = L.icon({
     //   iconUrl : 'https://linkedconnections.org/images/marker-icon.png',
     //   iconAnchor: [12, 41]
@@ -35,8 +37,8 @@ class InteractiveMap extends Component {
     //   iconUrl : 'https://linkedconnections.org/images/marker-icon-end.png',
     //   iconRetinaUrl: 'https://linkedconnections.org/images/marker-icon-2x-end.png'
     // });
-    // L.Icon.Default.iconUrl = 'https://linkedconnections.org/images/marker-icon.png';
-    // L.Icon.Default.iconRetinaUrl = 'https://linkedconnections.org/images/marker-icon-2x.png';
+    L.Icon.Default.iconUrl = 'https://linkedconnections.org/images/marker-icon.png';
+    L.Icon.Default.iconRetinaUrl = 'https://linkedconnections.org/images/marker-icon-2x.png';
 
     window.fetch("https://irail.be/stations/NMBS", {headers: {'accept': 'application/ld+json'}}).then(function(response) {
       return response.json();
@@ -45,9 +47,11 @@ class InteractiveMap extends Component {
         const key = station["@id"];
         station.point = new L.LatLng(station.latitude, station.longitude);
         stations[key] = station;
-        markers[key] = L.marker([station.latitude, station.longitude]).addTo(map);
+        markers[key] = L.marker([station.latitude, station.longitude]);
         markers[key].bindPopup(station.name);
+        markerLayer.addLayer(markers[key]);
       });
+
       window.fetch("https://belgium.linkedconnections.org/delijn/Oost-Vlaanderen/stops").then(function(response) {
         return response.json();
       }).then(function (stopsDeLijn) {
@@ -56,6 +60,8 @@ class InteractiveMap extends Component {
     }).catch(function(ex) {
       console.error(ex);
     });
+
+    map.addLayer(markerLayer);
   }
 
   render() {
--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 
-const leafletJS = document.createElement("script");
-leafletJS.setAttribute("href", "../node_modules/leaflet/dist/leaflet.js");
-document.getElementsByTagName("head")[0].appendChild(leafletJS);
+const leaflet = document.createElement("script");
+leaflet.setAttribute("src", "../node_modules/leaflet/dist/leaflet.js");
+document.getElementsByTagName("head")[0].appendChild(leaflet);
+
+const leafletCluster = document.createElement("script");
+leafletCluster.setAttribute("src", "../node_modules/leaflet.markercluster/dist/leaflet.markercluster-src.js");
+document.getElementsByTagName("head")[0].appendChild(leafletCluster);
